fad94a443ed41c8b1a2aeb0a6feb365f6c5e755f
gingus55 <gingus55@hotmail.com>
2021-11-27 19:10:15 +0000

    api notes added

---
+++ api/notes.js
@@ -0,0 +1,17 @@
+const getNotes = () => {
+  console.log("get the notes");
+};
+
+const createNotes = () => {
+  console.log("create the notes");
+};
+
+const deleteNotes = () => {
+  console.log("delete a note");
+};
+
+module.exports = {
+  getNotes,
+  createNotes,
+  deleteNotes,
+};
